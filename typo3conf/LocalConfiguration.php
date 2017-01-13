b7d458e1864959e151e59ade3d34603b7d94b4d0
Formation <formation@can.local>
2017-01-13 11:53:18 +0100

    rsa2

--- typo3conf/LocalConfiguration.php
+++ typo3conf/LocalConfiguration.php
@@ -58,7 +58,7 @@ return [
     ],
     'FE' => [
         'debug' => false,
-        'loginSecurityLevel' => 'rsa',
+        'loginSecurityLevel' => 'rsa2',
     ],
     'GFX' => [
         'colorspace' => 'RGB',
